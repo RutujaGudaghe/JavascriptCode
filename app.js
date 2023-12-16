40981b55bc323b3db17d12a471f165c3c8ca55ad
RutujaGudaghe <rutujagudghe@gmail.com>
2023-12-16 15:13:45 +0530

    textarea, button ,onclick listener

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-var temp= prompt("give me your name");
+var btnTranslate= document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input")
 
-alert("works correct!", temp);
+function clickEventHandler(){
+    console.log("clicked");
+    console.log("input",txtInput.value);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
