cda2d3ddda15aed9c76e482d2132c1cbcd138250
RutujaGudaghe <rutujagudghe@gmail.com>
2023-12-17 13:17:21 +0530

    added url fetch call error handler in javascript

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var btnTranslate= document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input")
-var textArea= document.querySelector("textarea");
+/*var textArea= document.querySelector("textarea");*/
 var outputDiv=document.querySelector("#output");
 /*outputDiv.innerText="rutuja is smart"*/
+
+var serverURL= "https://api.funtranslations.com/translate/minion.json"
+ function urlTranslate(text){
+    return serverURL + "?" +"text="+text;
+ }
+ function errorHandler(){
+console.log("error occured", error);
+}
 function clickEventHandler(){
     
-    outputDiv.innerText="Input isnrnrn"+ txtInput.value;
+    inputText=txtInput.value;
+    fetch(urlTranslate(inputText))
+    .then(response => response(json))
+    .then(json => { var translatedText=json.contents.translated;
+                   outoutDiv.innerText=translatedText;
+    })
+    .catch(errorHandler)
+
 }
-console.log(outputDiv.value)
+/*console.log(outputDiv.value)*/
 btnTranslate.addEventListener("click", clickEventHandler);
